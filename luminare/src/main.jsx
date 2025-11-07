0d91b0fbe1fa9d2fef1ff9b306f5abb22090b419
João Polezi <joao.francisco12123@gmail.com>
2025-11-07 10:00:31 -0300

    fix for one thousand time

--- luminare/src/main.jsx
+++ luminare/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router";
-import router from "./routes"; 
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
 import { GlobalStyle } from "./styled";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- luminare/src/routes.jsx
+++ luminare/src/routes.jsx
@@ -1,30 +1,27 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/home/Home";
 import Follow from "./pages/follow/Follow";
 import Games from "./pages/games/Games";
 
-
 const router = createBrowserRouter([
-    {
-        element: <Layout />,
-        children: [
-        {
-            index: true,
-            element: <Home />
-        },
-        {
-            path: "/acompanhamento",
-            element: <Follow />
-        },
-        {
-            path: "/gamificacao",
-            element: <Games />
-        }
-
-        ]
-    }
-
+  {
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "/acompanhamento",
+        element: <Follow />
+      },
+      {
+        path: "/gamificacao",
+        element: <Games />
+      }
+    ]
+  }
 ]);
 
 export default router;
